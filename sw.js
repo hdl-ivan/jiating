6d45bc9bcfa51af17b0110edf40c48fad3647d8c
hdl-ivan <hsieh.dailin@gmail.com>
2026-08-07 09:55:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 家庭記帳 Service Worker
    策略：network-first（確保 iOS 主畫面 App 一定拿得到最新版），離線時退回快取 */
-const CACHE = 'jiating-v4-4-3';
+const CACHE = 'jiating-v4-4-4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('message', e => {
@@ -32,4 +32,3 @@ self.addEventListener('fetch', e => {
     }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
- 
