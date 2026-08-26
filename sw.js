d16a6ab2cef5b84ab30eae77ed0462b168699773
hdl-ivan <hsieh.dailin@gmail.com>
2026-08-26 22:29:44 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 家庭記帳 Service Worker
    策略：network-first（確保 iOS 主畫面 App 一定拿得到最新版），離線時退回快取 */
-const CACHE = 'jiating-v6-15-1';
+const CACHE = 'jiating-v6-16-0';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './sortable.min.js'];
 
 self.addEventListener('message', e => {
